c360f30c8c858e44a6d46c88d57c9ebce4d030fd
Caio <caiot.seares@gmail.com>
2026-03-26 15:57:20 -0300

    feat(models): add model de TipoSanguineo

--- src/config/database-connection.js
+++ src/config/database-connection.js
@@ -3,30 +3,67 @@ import { databaseConfig } from "./database-config.js";
 
 import { Uf } from '../models/Uf.js';
 import { Cidade } from '../models/Cidade.js';
+import { TipoSanguineo } from '../models/TipoSanguineo.js';
 
 const sequelize = new Sequelize(databaseConfig);
 
 Uf.init(sequelize);
 Cidade.init(sequelize);
+TipoSanguineo.init(sequelize);
 
 Uf.associate(sequelize.models);
 Cidade.associate(sequelize.models);
+TipoSanguineo.associate(sequelize.models);
 
 databaseInserts(); // comentar quando estiver em ambiente de produção (não criar tabelas e não inserir registros de teste)
 
 function databaseInserts() {
     (async () => {
 
-        await sequelize.sync({ force: true }); 
+        await sequelize.sync({ force: true });
 
+        // Cadastro de UFs
         const uf1 = await Uf.create({ sigla: "ES", nome: "Espírito Santo" });
         const uf2 = await Uf.create({ sigla: "MG", nome: "Minas Gerais" });
+
 
         const cidade1 = await Cidade.create({ nome: "Cachoeiro", habitantes: 210000, area: 876.8, ufId: 1 });
         const cidade2 = await Cidade.create({ nome: "Alegre", habitantes: 31000, area: 772.0, ufId: 1 });
         const cidade3 = await Cidade.create({ nome: "Belo Horizonte", habitantes: 2520000, area: 331.4, ufId: 2 });
         const cidade4 = await Cidade.create({ nome: "Lavras", habitantes: 105000, area: 564.7, ufId: 2 });
 
+        const tipos = await Promise.all([
+            TipoSanguineo.create({
+                grupoABO: 'A',
+                fatorRH: true,
+                quantidade: 120,
+                descricao: 'Tipo Sanguíneo A positivo'
+            }),
+            TipoSanguineo.create({
+                grupoABO: 'A',
+                fatorRH: false,
+                quantidade: 80,
+                descricao: 'Tipo Sanguíneo A negativo'
+            }),
+            TipoSanguineo.create({
+                grupoABO: 'B',
+                fatorRH: true,
+                quantidade: 60,
+                descricao: 'Tipo Sanguíneo B positivo'
+            }),
+            TipoSanguineo.create({
+                grupoABO: 'O',
+                fatorRH: true,
+                quantidade: 200,
+                descricao: 'Tipo Sanguíneo O positivo'
+            }),
+            TipoSanguineo.create({
+                grupoABO: 'AB',
+                fatorRH: true,
+                quantidade: 40,
+                descricao: 'Tipo Sanguíneo AB positivo'
+            })
+        ]);
     })();
 }
 
---
+++ src/models/TipoSanguineo.js
@@ -0,0 +1,42 @@
+import { Model, DataTypes } from 'sequelize';
+
+class TipoSanguineo extends Model {
+
+  static init(sequelize) {
+    super.init({
+      grupoABO: { 
+        type: DataTypes.STRING, 
+        allowNull: false,
+        validate: {
+          notEmpty: { msg: "Grupo ABO deve ser preenchida!" },
+          len: { args: [1, 2], msg: "Grupo ABO deve ter entre 1 e 2 letras!" }
+        }
+      },
+      fatorRH: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        validate: {
+          notNull: { msg: "Fator RH deve ser informado!" }
+        }
+      },
+      quantidade: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          isInt: { msg: "Quantidade deve ser um número inteiro!" },
+          min: { args: [0], msg: "Quantidade não pode ser negativa!" }
+        }
+      },
+      descricao: {
+        type: DataTypes.STRING,
+        allowNull: true
+      }
+    }, { sequelize, modelName: 'tipoSanguineo', tableName: 'tipos_sanguineos' })
+  }
+
+  static associate(models) {
+  }
+  
+}
+
+export { TipoSanguineo };

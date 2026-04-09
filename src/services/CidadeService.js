eb5a392e94aea0a59f8855d687f8dea20d0eff0c
gabrielamqs <gabriela.benevidespm@gmail.com>
2026-04-09 17:41:05 -0300

    ✨ feat: Implementação do Controller e Service para a classe Cidade

---
+++ src/services/CidadeService.js
@@ -0,0 +1,53 @@
+import { Cidade } from '../models/Cidade.js';
+
+class CidadeService {
+
+    static async findAll() {
+        const objs = await Cidade.findAll({ include: { all: true, nested: true } });
+        return objs;
+    }
+
+    static async findByUf(req) {
+        const { id } = req.params;
+        const objs = await Cidade.findAll({ where: { ufId: id }, include: { all: true, nested: true } });
+        return objs;
+    }
+
+    static async findByPk(req) {
+        const { id } = req.params;
+        const obj = await Cidade.findByPk(id, { include: { all: true, nested: true } });
+        return obj;
+    }
+
+    static async create(req) {
+        const { nome, area, habitantes, uf } = req.body;
+        if (uf == null) throw 'A Uf da Cidade deve ser preenchida!';
+        const obj = await Cidade.create({ nome, area, habitantes, ufId: uf.id });
+        return await Cidade.findByPk(obj.id, { include: { all: true, nested: true } });
+    }
+
+    static async update(req) {
+        const { id } = req.params;
+        const { nome, area, habitantes, uf } = req.body;
+        if (uf == null) throw 'A Uf da Cidade deve ser preenchida!';
+        const obj = await Cidade.findByPk(id, { include: { all: true, nested: true } });
+        if (obj == null) throw 'Cidade não encontrada!';
+        Object.assign(obj, { nome, area, habitantes, ufId: uf.id });
+        await obj.save();
+        return await Cidade.findByPk(obj.id, { include: { all: true, nested: true } });
+    }
+
+    static async delete(req) {
+        const { id } = req.params;
+        const obj = await Cidade.findByPk(id);
+        if (obj == null) throw 'Cidade não encontrada!';
+        try {
+            await obj.destroy();
+            return obj;
+        } catch (error) {
+            throw "Não é possível remover uma Cidade que possui dados relacionados!";
+        }
+    }
+}
+
+export { CidadeService };

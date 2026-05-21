44c4a5ba2ede10a20bb8edee0b9cceb9c3e31beb
Caio <caiot.seares@gmail.com>
2026-05-21 14:30:07 -0300

    feat: adicionando conexão com banco remoto no render

--- src/config/database-config.js
+++ src/config/database-config.js
@@ -1,14 +1,14 @@
 
 // Configuração do banco de dados no ambiente de teste
-export const databaseConfig = {
-  dialect: 'sqlite',
-  storage: 'database.sqlite',
-  define: {
-    timestamps: true,
-    freezeTableName: true,
-    underscored: true
-  }
-};
+// export const databaseConfig = {
+//   dialect: 'sqlite',
+//   storage: 'database.sqlite',
+//   define: {
+//     timestamps: true,
+//     freezeTableName: true,
+//     underscored: true
+//   }
+// };
 
 /*
 // Configuração do banco de dados no ambiente de desenvolvimento
@@ -26,18 +26,20 @@ export const databaseConfig = {
 };
 */
 
-/*
+
 // Configuração do banco de dados no ambiente de produção
 export const databaseConfig = {
   dialect: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: 'postgres',
-  database: 'scv-backend-node-sequelize',
+  host: 'dpg-d87jm6h9rddc73fq5u7g-a.virginia-postgres.render.com',
+  username: 'scds_backend_node_sequelize_user',
+  password: 'kCjajY6GcjIbpiUZn6UOv5i6eedNeW6r',
+  database: 'scds_backend_node_sequelize',
   define: {
     timestamps: true,
     freezeTableName: true,
     underscored: true
+  },
+  dialectOptions: {
+    ssl: true
   }
 };
-*/
